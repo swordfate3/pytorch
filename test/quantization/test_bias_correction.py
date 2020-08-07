7eea19396bcc57c8abf9aca15ccc47865f556a44
Edmund Williams Jr <edmundjr@fb.com>
2020-08-07 13:42:50 -0700

    demo sutff
    ghstack-source-id: 751769e28e73ea5170ff79fbb23965d6be1e7184
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/41196

---
+++ test/quantization/test_bias_correction.py
@@ -0,0 +1,98 @@
+import torch
+import torch.nn as nn
+from torch.testing._internal.common_quantization import QuantizationTestCase
+from torch.quantization import QuantStub, DeQuantStub
+import torch.quantization._correct_bias as _correct_bias
+from torch.quantization._correct_bias import _supported_modules, _supported_modules_quantized
+from torchvision.models.quantization import mobilenet_v2
+import copy
+from torch.quantization import default_qconfig
+
+class TestBiasCorrection(QuantizationTestCase):
+    def compute_sqnr(self, x, y):
+        Ps = torch.norm(x)
+        Pn = torch.norm(x - y)
+        return 20 * torch.log10(Ps / Pn)
+
+    def correct_artificial_bias(self, float_model, bias_correction, img_data):
+        ''' Adding artificial bias and testing if bias persists after bias
+            correction
+        '''
+        artificial_model = copy.deepcopy(float_model)
+        artificial_model.qconfig = default_qconfig
+        torch.quantization.prepare(artificial_model, inplace=True)
+        artificial_model(img_data[0][0])
+        torch.quantization.convert(artificial_model, inplace=True)
+
+        # manually changing bias
+        for name, submodule in artificial_model.named_modules():
+            if type(submodule) in _supported_modules_quantized:
+                x = _correct_bias.get_param(submodule, 'bias')
+                if x is not None:
+                    x.data = x.data * 10
+
+        bias_correction(float_model, artificial_model, img_data)
+
+        for name, artificial_submodule in artificial_model.named_modules():
+            if type(artificial_submodule) in _correct_bias._supported_modules_quantized:
+                submodule = _correct_bias.get_module(float_model, name)
+                float_bias = _correct_bias.get_param(submodule, 'bias')
+                artificial_bias = _correct_bias.get_param(artificial_submodule, 'bias')
+
+                if artificial_submodule in _supported_modules:
+                    if artificial_bias.is_quantized:
+                        artificial_bias = artificial_bias.dequantize()
+                    self.assertTrue(self.computeSqnr(float_bias, artificial_bias) > 35,
+                                    "Correcting quantized bias produced too much noise, sqnr score too low")
+
+    def test_linear_chain(self):
+        class LinearChain(nn.Module):
+            def __init__(self):
+                super(LinearChain, self).__init__()
+                self.linear1 = nn.Linear(3, 4)
+                self.linear2 = nn.Linear(4, 5)
+                self.linear3 = nn.Linear(5, 6)
+                self.quant = QuantStub()
+                self.dequant = DeQuantStub()
+
+            def forward(self, x):
+                x = self.quant(x)
+                x = self.linear1(x)
+                x = self.linear2(x)
+                x = self.linear3(x)
+                x = self.dequant(x)
+                return x
+        float_model = LinearChain()
+        img_data = [(torch.rand(10, 3, dtype=torch.float), torch.randint(0, 1, (2,), dtype=torch.long))
+                    for _ in range(50)]
+        self.correct_artificial_bias(float_model, _correct_bias.bias_correction, img_data)
+
+    def test_conv_chain(self):
+        class ConvChain(nn.Module):
+            def __init__(self):
+                super(ConvChain, self).__init__()
+                self.conv2d1 = nn.Conv2d(3, 4, 5, 5)
+                self.conv2d2 = nn.Conv2d(4, 5, 5, 5)
+                self.conv2d3 = nn.Conv2d(5, 6, 5, 5)
+                self.quant = QuantStub()
+                self.dequant = DeQuantStub()
+
+            def forward(self, x):
+                x = self.quant(x)
+                x = self.conv2d1(x)
+                x = self.conv2d2(x)
+                x = self.conv2d3(x)
+                x = self.dequant(x)
+                return x
+        float_model = ConvChain()
+        img_data = [(torch.rand(10, 3, 125, 125, dtype=torch.float), torch.randint(0, 1, (2,), dtype=torch.long))
+                    for _ in range(50)]
+        self.correct_artificial_bias(float_model, _correct_bias.bias_correction, img_data)
+
+    def test_mobilenet(self):
+        float_model = mobilenet_v2(pretrained=True, quantize=False)
+        float_model.eval()
+        float_model.fuse_model()
+        img_data = [(torch.rand(10, 3, 224, 224, dtype=torch.float), torch.randint(0, 1, (2,), dtype=torch.long))
+                    for _ in range(50)]
+        self.correct_artificial_bias(float_model, _correct_bias.bias_correction, img_data)
---
+++ torch/quantization/_correct_bias.py
@@ -0,0 +1,121 @@
+from __future__ import print_function, division, absolute_import
+import torch
+import torch.nn as nn
+import torch.nn.quantized as nnq
+
+import torch.quantization
+import torch.quantization._numeric_suite as ns
+
+_supported_modules = {nn.Linear, nn.Conv2d}
+_supported_modules_quantized = {nnq.Linear, nnq.Conv2d}
+
+def get_module(model, name):
+    ''' Given name of submodule, this function grabs the submodule from given model
+    '''
+    current = model
+    name = name.split('.')
+    for subname in name:
+        if subname == '':
+            return current
+        current = current._modules[subname]
+    return current
+
+def get_parent_module(model, name):
+    ''' Given name of submodule, this function grabs the parent of the submodule, from given model
+    '''
+    parent_name = name.rsplit('.', 1)[0]
+    if parent_name == name:
+        parent_name = ''
+    return get_module(model, parent_name)
+
+def get_param(module, attr):
+    ''' Sometimes the weights/bias attribute gives you the raw tensor, but sometimes
+    gives a function that will give you the raw tensor, this function takes care of that logic
+    '''
+    param = getattr(module, attr, None)
+    if isinstance(param, nn.Parameter) or isinstance(param, torch.Tensor):
+        return param
+    elif callable(param):
+        return param()
+    return None
+
+class MeanShadowLogger(ns.Logger):
+    r"""A logger for a Shadow module whose purpose is to record the rolling mean
+    of the data passed to the floating point and quantized models
+    """
+    def __init__(self):
+        super(MeanShadowLogger, self).__init__()
+        self.stats["float"] = None
+        self.stats["quantized"] = None
+        self.count = 0
+        self.float_sum = None
+        self.quant_sum = None
+
+    def forward(self, x, y):
+        if len(x) > 1:
+            x = x[0]
+        if len(y) > 1:
+            y = y[0]
+        if x.is_quantized:
+            x = x.dequantize()
+
+        self.count += 1
+        if self.stats["quantized"] is None:
+            self.stats["quantized"] = x
+            self.quant_sum = x
+        else:
+            self.quant_sum += x
+            self.stats["quantized"] = self.quant_sum / self.count
+
+        if self.stats["float"] is None:
+            self.stats["float"] = y
+            self.float_sum = y
+        else:
+            self.float_sum += y
+            self.stats["float"] = self.float_sum / self.count
+
+def bias_correction(float_model, quantized_model, img_data, neval_batches=30):
+    ''' Using numeric suite shadow module, the expected output of the floating point and quantized modules
+    is recorded. Using that data the bias of supported modules is shifted to compensate for the drift caused
+    by quantization
+    Paper reference: https://arxiv.org/pdf/1906.04721.pdf (Section 4.2)
+    '''
+    biased_modules = {}
+    for name, submodule in quantized_model.named_modules():
+        if type(submodule) in _supported_modules_quantized:
+            biased_modules[name] = submodule
+
+    for biased_module in biased_modules:
+        quantized_submodule = get_module(quantized_model, biased_module)
+        bias = get_param(quantized_submodule, 'bias')
+        if bias is not None:
+            ns.prepare_model_with_stubs(float_model, quantized_model, _supported_modules, MeanShadowLogger)
+            count = 0
+            for data in img_data:
+                quantized_model(data[0])
+                count += 1
+                if count % 10 == 0:
+                    print('.', end='')  # keeps devserver open
+                if count == neval_batches:
+                    break
+            ob_dict = ns.get_logger_dict(quantized_model)
+
+            float_data = ob_dict[biased_module + '.stats']['float']
+            quant_data = ob_dict[biased_module + '.stats']['quantized']
+
+            # math for expected_error
+            quantization_error = quant_data - float_data
+            dims = list(range(1, quantization_error.dim()))
+            expected_error = torch.mean(quantization_error, dims)
+
+            updated_bias = bias.data - expected_error
+            updated_bias = updated_bias.reshape(bias.data.size())
+
+            bias.data = updated_bias
+
+            # Removing shadows from model, needed to prevent nesting of shadow modules
+            for name, submodule in quantized_model.named_modules():
+                if isinstance(submodule, ns.Shadow):
+                    parent = get_parent_module(quantized_model, name)
+                    child_name = name.rsplit('.', 1)[-1]
+                    parent._modules[child_name] = submodule.orig_module
